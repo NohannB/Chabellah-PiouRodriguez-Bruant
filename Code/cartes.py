d58bf4fe5dcc01bfcb76f286faa0d5016c05e391
Gabriel <gbl.piou@gmail.com>
2026-04-21 16:29:33 +0200

    Modification cartes.py

--- Code/cartes.py
+++ Code/cartes.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import random
 
+# Classe abstraite générale
+
 
 class Carte(ABC):
 
@@ -8,14 +10,62 @@ class Carte(ABC):
         self.visuel = None
         self.nom = None
 
+    @abstractmethod
+    def activation_carte(self):
+        pass
 
-class CartePassive(Carte, ABC):
+
+# Cartes actives
+class CarteTourner(Carte):
 
     def __init__(self):
         super().__init__()
+        self.cooldown = None
+        self.count_cooldown = None
+
+    def activation_carte(self):
+        pass
 
 
-class CarteActive(Carte, ABC):
+class CarteSymetrie(Carte):
 
     def __init__(self):
         super().__init__()
+        self.cooldown = None
+        self.count_cooldown = None
+
+    def activation_carte(self):
+        pass
+
+
+class CarteChangerPolyomino(Carte):
+
+    def __init__(self):
+        super().__init__()
+        self.cooldown = 2
+        self.count_cooldown = 0
+
+    def activation_carte(self):
+        pass
+
+
+class CarteLibererCasePolluee(Carte):
+
+    def __init__(self):
+        super().__init__()
+        self.cooldown = 3
+        self.count_cooldown = 0
+
+    def activation_carte(self):
+        pass
+
+
+class CarteVoirPolyominoSuivant(Carte):
+
+    def __init__(self):
+        super().__init__()
+        self.cooldown = 2
+        self.count_cooldown = 0
+
+    def activation_carte(self):
+        pass
